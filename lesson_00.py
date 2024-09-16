05fe94c1ca7325a3a7e7b829c346912c27cb2e97
Christian Alleyne <chrisalleyne3@gmail.com>
2024-09-16 10:47:28 -0400

    Python syntax

---
+++ lesson_00.py
@@ -0,0 +1,47 @@
+#Always put everything in snake case
+
+#Step 1 Print name
+print("Christian Alleyne")
+
+#Step 2 download python extention
+
+# Step 3 create a variable called favorate show
+
+x=("naruto")
+
+#step 4 create one float and 1 integer
+
+y=2
+z=3.5
+
+#Step 5 multiply the float and integer
+
+print(y*z)
+
+#Step 6 Create a variable of at least 3 favorate foods
+
+a=('Pizza')
+b=('burger')
+c=('ramen')
+
+#Step 7 make an if statement to tell someone if they can get a driver's lisense
+
+perm=input("Do you have a driver's permit?")
+
+if (perm == "yes"):
+    print("Yes you may")
+else:
+    print("No")
+
+#Step 8 Ask user their name
+
+name=input("What is your name?")
+print(name)
+
+#step 9 create a for loop that prints all of my favorate foods
+
+
+#Step 10 create a while loop that will nover run 
+i = 0
+while i == 6:
+  print(i)
