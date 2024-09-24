4b4b3d3093d0b7c08e590f0cbbfa5bee1ae0799d
Christian Alleyne <chrisalleyne3@gmail.com>
2024-09-24 10:51:50 -0400

    almost done lesson 6

---
+++ lesson_06.py
@@ -0,0 +1,43 @@
+"""
+1. Simple Counter:
+Write a program that uses a while loop to print numbers from 1 to 10.
+"""
+i=0
+while i<10:
+    i+=1
+    print(i)
+"""
+2. Countdown:
+Write a program that uses a while loop to print numbers from 10 to 1 in descending order.
+"""
+a=11
+while a>1:
+    a-=1
+    print(a)
+"""
+3. Factorial Calculation:
+Write a program that calculates the factorial of a given number using a while loop.
+"""
+x=float(input('choose a number and I will do a factorial equation '))
+y=10
+while y>=1:
+    x=x*y
+    y-=1
+
+print(x)
+"""
+4. Password Guessing Game:
+Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
+"""
+word="boruto"
+
+"""
+5. Sum of Digits:
+Write a program that calculates the sum of the digits of a given number using a while loop.
+"""
+
+
+"""
+6. Fibonacci Series:
+Write a program that prints the first n numbers in the Fibonacci sequence using a while loop.
+"""
