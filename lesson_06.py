164ccca97cdc541c877e4eacdeb261faa76f753a
Christian Alleyne <chrisalleyne3@gmail.com>
2024-09-25 10:06:52 -0400

    password

--- lesson_06.py
+++ lesson_06.py
@@ -6,6 +6,7 @@ i=0
 while i<10:
     i+=1
     print(i)
+# Made i=0 so that 10 would be counted then made a while loop that added 1 and stopped at 10
 """
 2. Countdown:
 Write a program that uses a while loop to print numbers from 10 to 1 in descending order.
@@ -14,6 +15,7 @@ a=11
 while a>1:
     a-=1
     print(a)
+# I reversed 1
 """
 3. Factorial Calculation:
 Write a program that calculates the factorial of a given number using a while loop.
@@ -30,6 +32,16 @@ print(x)
 Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
 """
 word="boruto"
+guess=input("What is your password? ")
+
+while guess!=word:
+    print('incorrect. The password is the name of narutos first child')
+    break
+if guess == word:
+    print('correct')
+
+
+
 
 """
 5. Sum of Digits:
