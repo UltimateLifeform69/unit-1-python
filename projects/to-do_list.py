7ab674d1a08823461dd9ab60472eccb659e120eb
Christian Alleyne <chrisalleyne3@gmail.com>
2024-09-30 10:30:09 -0400

    To do list (save and quit)

--- projects/to-do_list.py
+++ projects/to-do_list.py
@@ -1,19 +1,25 @@
 print("Welcome to My To-Do Tracker")
 
-list=[]
+with open('projects/to-do.txt') as file:
+    list = file.readlines()
 
 while True:
     x=str(input('''Would you like to add or remove an item from your list 
     1 = Add
     2 = Remove
+    3 = save and quit
     '''))
 
 
 
     if x == '1':
-        list.append(input('What would you like to add? ').strip())
+        list.append(input('What would you like to add? ' + '\n').strip())
     elif x == '2':
         list.remove(input('What would you like to remove? ').strip())
+    elif x == '3':
+        with open('projects/to-do.txt' , 'w') as file:
+            file.writelines(list)
+        break
     else:
         print('That is invalid')
 
